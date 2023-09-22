5b70fc1b501f6571e37f625eb7d927baa8f6fa7f
Kyle Thompson <kt@deeper.io>
2023-09-21 22:52:59 -0500

    started impl for reporting service

--- server/src/master/services/reporting.rs
+++ server/src/master/services/reporting.rs
@@ -1,31 +1,57 @@
 use crate::master::managers::storage::ThreadSafeStorageManager;
 use crate::metalfs::master_reporting_service_server as mrss;
-use crate::metalfs::{ReportChunkRequest, ReportChunkResponse};
+use crate::metalfs::{ReportStorageServerRequest, ReportStorageServerResponse};
 use mrss::{MasterReportingService, MasterReportingServiceServer};
 use std::sync::Arc;
+use tokio::sync::RwLock;
 use tonic::{Request, Response, Status};
 
 pub(crate) fn make_reporting_server(
-    _: Arc<ThreadSafeStorageManager>,
+    storage_mgr: Arc<ThreadSafeStorageManager>,
 ) -> MasterReportingServiceServer<Service> {
-    MasterReportingServiceServer::new(Service::new())
+    MasterReportingServiceServer::new(Service::new(storage_mgr))
 }
 
-#[derive(Default)]
-pub(crate) struct Service {}
+pub(crate) struct Service {
+    // Storage manager
+    storage_mgr: Arc<ThreadSafeStorageManager>,
+}
 
 impl Service {
-    fn new() -> Self {
-        Service {}
+    fn new(storage_mgr: Arc<ThreadSafeStorageManager>) -> Self {
+        Service { storage_mgr }
     }
 }
 
 #[tonic::async_trait]
 impl MasterReportingService for Service {
-    async fn report_chunk(
+    async fn report_storage_server(
         &self,
-        _: Request<ReportChunkRequest>,
-    ) -> Result<Response<ReportChunkResponse>, Status> {
-        todo!()
+        req: Request<ReportStorageServerRequest>,
+    ) -> Result<Response<ReportStorageServerResponse>, Status> {
+        let req = req.into_inner();
+
+        let Some(server) = req.storage_server.clone() else {
+            return Err(Status::invalid_argument("storage_server argument is required"));
+        };
+
+        let Some(location) = server.location.clone() else {
+            return Err(Status::invalid_argument("server location argument is required"));
+        };
+
+        let existing = self.storage_mgr.get_server(&location).await;
+
+        if existing.is_none() {
+            self.storage_mgr
+                .register_server(Arc::new(RwLock::new(server)))
+                .await;
+
+            return Ok(Response::new(ReportStorageServerResponse {
+                request: Some(req),
+                stale_chunk_handles: vec![],
+            }));
+        }
+
+        todo!("update existing server");
     }
 }
